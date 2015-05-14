6a599d635cc2bddaa3d68f496c0f88f1f7ea32cc
JayaDarby <jayadarby@gmail.com>
2015-05-13 18:59:07 -0700

    ierughe

--- script.js
+++ script.js
@@ -8,10 +8,17 @@ for(var i = 0; i <81; i++){
 	  myDiv.style.backgroundColor='red';
 	else
 	  myDiv.style.backgroundColor='black';
-    ///eiuhgrirushg
-    //seuhgousrhtt
-    //slejgourtjh
-    //sljrtoijrt
+
 	document.body.appendChild(myDiv);
 }
 
+
+for(var i = 0; i <81; i++){
+	var myDiv = document.createElement("div");
+	myDiv.style.width = "11.1%";
+	myDiv.style.float = "left";
+	myDiv.style.paddingBottom = '11.1%';
+	myDiv.style.backgroundColor='"#" + Math.random().toString(16).slice(2, 8)';
+	
+	document.body.appendChild(myDiv);
+}
