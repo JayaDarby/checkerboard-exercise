4b734326afbd46828bac60ddb4e5273b3bc6153f
JayaDarby <jayadarby@gmail.com>
2015-05-13 18:36:13 -0700

    added checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Your JS goes here
+for(var i = 0; i <81; i++){
+	var myDiv = document.createElement("div");
+	myDiv.style.width = "11.1%";
+	myDiv.style.float = "left";
+	myDiv.style.paddingBottom = '11.1%';
+	if(i%2===0)
+	  myDiv.style.backgroundColor='red';
+	else
+	  myDiv.style.backgroundColor='black';
+
+	document.body.appendChild(myDiv);
+}
+
